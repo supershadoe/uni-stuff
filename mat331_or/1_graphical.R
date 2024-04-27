f5ce5ca0cdf3c92653bfec79f2e741829b12a503
nightly5 <120316532+nightly5@users.noreply.github.com>
2024-04-27 09:03:08 +0530

    Fix OR Code

--- mat331_or/1_graphical.R
+++ mat331_or/1_graphical.R
@@ -1,9 +1,43 @@
+library(ggplot2)
 library(lpSolve)
 
-objective <- c(4, 10)
-constraints <- matrix(c(2, 1, 2, 5, 2, 3), nrow=3, byrow=TRUE)
+colors.lavender <- "#CA87FF";
+colors.grass <- "#A2D290";
+colors.aqua <- "#00AAAA";
+
+feasible_region <- as.data.frame(matrix(
+  c(0, 10/3, 0, 0, 2, 0, 4, 2, 2, 4),
+  ncol = 2,
+  byrow = TRUE
+));
+colnames(feasible_region) <- c('x', 'y');
+
+p <- ggplot(data=data.frame(x = 0), aes(x = x)) +
+  scale_x_continuous(breaks = seq(0, 10, 1),lim = c(0, 10)) +
+  scale_y_continuous(breaks = seq(0, 10, 1),lim = c(0, 10)) +
+  geom_vline(xintercept = 0, color = colors.aqua) +
+  geom_hline(yintercept = 0, color = colors.aqua) +
+  geom_abline(slope = 1/3, intercept= 10/3, color = colors.lavender) +
+  geom_abline(slope = -1, intercept = 6, color = colors.lavender) +
+  geom_abline(slope = 1, intercept = -2, color = colors.lavender) +
+  geom_polygon(
+    data = feasible_region,
+    mapping = aes(x = x, y = y),
+    fill = colors.grass,
+  ) +
+  geom_point(data = feasible_region, aes(x = x, y = y), color = colors.aqua) +
+  labs(
+      title = 'Optimization Problem',
+      subtitle = 'Graphical Method',
+      x = 'x1',
+      y = 'x2',
+    )
+print(p)
+
+objective <- c(-1, 2)
+constraints <- matrix(c(-1, 3, 1, 1, 1, -1), ncol = 2, byrow = TRUE)
 signs <- c('<=', '<=', '<=')
-rhs <- c(50, 100, 90)
+rhs <- c(10, 6, 2)
 
 res <- lp(
   objective.in = objective,
@@ -13,22 +47,13 @@ res <- lp(
   direction = 'max',
 )
 
-cons <- as.data.frame(t(res$constraints))[, c('V1', 'V2')]
-colnames(cons) <- c('x', 'y')
-
 sol <- as.data.frame(rbind(res$solution))
 colnames(sol) <- c('x', 'y')
 
-library(ggplot2)
-p <- ggplot(data=as.data.frame(res$solution), aes(x = x, y = y)) +
-  geom_vline(xintercept = 0) +
-  geom_hline(yintercept = 0) +
-  geom_point(data=sol) +
-  geom_point(data=cons) +
-  labs(
-      title = 'Optimization Problem',
-      subtitle = 'Graphical Method',
-      x = 'x1',
-      y = 'x2',
-    )
+p <- p + geom_point(
+  data = sol,
+  aes(x = x, y = y),
+  color = colors.lavender,
+  size = 3,
+)
 print(p)
--- mat331_or/3_two_phase.R
+++ mat331_or/3_two_phase.R
@@ -75,33 +75,3 @@ if (all(res$solution == 0)) {
 } else {
   print('Phase 1: No feasible solution possible.')
 }
-
-obj.ph1 <- c(0, 0, 0, 0, 0, -1, -1)
-var <- c('x1', 'x2', 's1', 's2', 's3', 'a1', 'a2')
-cons <- matrix(c(
-  2, 3, -1, 0, 0, 1,
-  1, 3, 0, 1, 0, 0,
-  0, 1, 0, 0, 1, 0
-), ncol = length(var), byrow = TRUE)
-signs <- c('>=', '<=', '<=')
-rhs <- c(2, 2, 4)
-
-res <- lp('min', obj.ph1, cons, signs, rhs)
-
-if (all(res$solution == 0)) {
-  obj.ph2 <- c(3, -1, 0, 0, 0)
-  var <- c('x1', 'x2', 's1', 's2', 's3')
-  cons <- matrix(c(
-    2, 1, -1, 0, 0,
-    1, 3, 0, 1, 0,
-    0, 1, 0, 0, 1
-  ), ncol = length(var), byrow = TRUE)
-  rhs <- c(2, 2, 4)
-  res <- lp('max', obj.ph2, cons, signs, rhs)
-  sol <- as.data.frame(rbind(res$solution))
-  colnames(sol) <- var
-  print(sol)
-  sprintf('Obj func val: %.2f', res$objval)
-} else {
-  print('Phase 1: No feasible solution possible.')
-}
